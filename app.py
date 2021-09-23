5464b7ac5a21139e34d1d3eca8ed67e196088b27
Akshit Madan <akmadan1717@gmail.com>
2021-09-23 14:47:49 +0530

    template inheritance

--- app.py
+++ app.py
@@ -9,6 +9,16 @@ def index():
     return render_template('index.html', name= 'Akshit', 
     answer = answer, passedList = namesList, user_logged_in=True) #String name and dict answer are passed to html file
 
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/puppy/<name>')
+def puppy(name):
+    return render_template('puppy.html', name=name)
+
 if __name__ == '__main__':
     app.run(debug=True)
  
