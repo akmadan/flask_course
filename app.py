0f07a296ad4d2fba44aee592dc9e8b718c63d169
Akshit Madan <akmadan1717@gmail.com>
2021-09-23 13:48:07 +0530

    control flow

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    answer = {'akshit': 'madan'}
+    namesList = ['Akshit', 'Aasif', 'Gaurav']
+    return render_template('index.html', name= 'Akshit', 
+    answer = answer, passedList = namesList, user_logged_in=True) #String name and dict answer are passed to html file
 
 if __name__ == '__main__':
     app.run(debug=True)
  
+
